58808029c86831a4d615d70f8229e674172db217
Jeison Ortiz <ortizjeison4@gmail.com>
2023-02-26 12:43:42 -0500

    first commit

---
+++ src/test/java/com/example/conectionexample/ConectionexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conectionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectionexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
